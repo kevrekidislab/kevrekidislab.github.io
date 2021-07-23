ec21742d83592f21a229300c95c502c60d7a5571
Tom Bertalan <tom@tombertalan.com>
2021-07-23 13:23:39 -0400

    Don't use .html in link to notebook rendering.

--- templates/index.py
+++ templates/index.py
@@ -8,9 +8,10 @@ notebooks_list_html = "<ul>"
 for notebook_ipynb in notebooks_list:
     notebook_ipynb = basename(notebook_ipynb)
     notebook_html = notebook_ipynb.replace(".ipynb", ".html")
+    notebook_base = basename(notebook_ipynb).replace(".ipynb", "")
     notebooks_list_html += (
         "\n    <li>"
-        + f"<a href='notebooks_html/{notebook_html}'>{notebook_html}</a>"
+        + f"<a href='notebooks_html/{notebook_html}'>{notebook_base}</a>"
         + f" | (<a href='notebooks/{notebook_ipynb}'>{notebook_ipynb}/a>)"
         + "</li>"
     )
